7b79b887929e022dfda2d911eefbd280873086f0
Moises Minrada <moisesboas@gmail.com>
2024-06-23 14:58:32 -0300

    feat: contructor test

--- src/route.entity.spec.ts
+++ src/route.entity.spec.ts
@@ -2,16 +2,23 @@ import { Route, RouteProps } from "./route.entity";
 
 describe("Route tests", () => {
   test("Constructor", () => {
-    const routeProps: RouteProps = {
+    let routeProps: RouteProps = {
       title: "Minha rota",
       startPosition: { lat: 0, lng: 1 },
       endPosition: { lat: 2, lng: 3 },
     };
 
-    const route = new Route(routeProps);
+    let route = new Route(routeProps);
     expect(route.props).toStrictEqual({
       ...routeProps,
       points: [],
+    });
+
+    routeProps.points = [{ lat: 10, lng: 11 }];
+    route = new Route(routeProps); // Calling again to resign the values.
+    expect(route.props).toStrictEqual({
+      ...routeProps,
+      points: [{ lat: 10, lng: 11 }],
     });
   });
 });
